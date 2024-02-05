0dfd5a31feb0d0ad714aaeecff9d815d785e0d45
EugeneChurylov <e.churylov@gmail.com>
2024-02-05 16:52:42 +0100

    pyramid update 2

--- 4.Pyramid/test.py
+++ 4.Pyramid/test.py
@@ -25,36 +25,6 @@ def add_edges(graph, node, pos, x=0, y=0, layer=1):
             r = add_edges(graph, node.right, pos, x=r, y=y - 1, layer=layer + 1)
     return graph
 
-def draw_tree(tree_root):
-    tree = nx.DiGraph()
-    pos = {tree_root.id: (0, 0)}
-    tree = add_edges(tree, tree_root, pos)
-
-    colors = [node[1]['color'] for node in tree.nodes(data=True)]
-    labels = {node[0]: node[1]['label'] for node in tree.nodes(data=True)}  # Використовуйте значення вузла для міток
-
-    plt.figure(figsize=(8, 5))
-    nx.draw(tree, pos=pos, labels=labels, arrows=False, node_size=2500, node_color=colors)
-    plt.show()
-
-def create_heap(nodes):
-    nodes.sort(reverse=True)  # Sort the nodes in descending order
-    if not nodes:
-        return None
-    root = Node(nodes[0])
-    queue = [root]
-    i = 1
-    while i < len(nodes):
-        current = queue.pop(0)
-        current.left = Node(nodes[i])
-        queue.append(current.left)
-        i += 1
-        if i < len(nodes):
-            current.right = Node(nodes[i])
-            queue.append(current.right)
-            i += 1
-    return root
-
 def draw_heap(heap_root):
     heap = nx.DiGraph()
     pos = {heap_root.id: (0, 0)}
@@ -67,23 +37,14 @@ def draw_heap(heap_root):
     nx.draw(heap, pos=pos, labels=labels, arrows=False, node_size=2500, node_color=colors)
     plt.show()
 
-# Get user input for the number of nodes
-num_nodes = int(input("Enter the number of nodes: "))
+# Створення бінарної купи
+heap_root = Node(20)
+heap_root.left = Node(17)
+heap_root.left.left = Node(15)
+heap_root.left.right = Node(10)
+heap_root.right = Node(5)
+heap_root.right.left = Node(3)
+heap_root.right.right = Node(1)
 
-# Get user input for node values
-node_values = [int(input(f"Enter the value for node {i + 1}: ")) for i in range(num_nodes)]
-
-# Creating the binary heap
-heap_root = create_heap(node_values)
-
-# Створення дерева
-root = Node(0)
-root.left = Node(4)
-root.left.left = Node(5)
-root.left.right = Node(10)
-root.right = Node(1)
-root.right.left = Node(3)
-
-# Displaying the binary heap
-draw_tree(root)
+# Відображення бінарної купи
 draw_heap(heap_root)
