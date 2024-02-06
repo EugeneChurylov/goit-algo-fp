60d51cdf00f0c25e7417c16f26d3d31ae29f3d2e
EugeneChurylov <e.churylov@gmail.com>
2024-02-06 16:10:58 +0100

    pep8 for the tree

--- 5.TreeStepsVisualization/main.py
+++ 5.TreeStepsVisualization/main.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from collections import deque
 from colorsys import hsv_to_rgb
 
+
 class Node:
     def __init__(self, key, color="skyblue"):
         self.left = None
@@ -12,32 +13,43 @@ class Node:
         self.color = color
         self.id = str(uuid.uuid4())
 
+
 def add_edges(graph, node, pos, x=0, y=0, layer=1):
     if node is not None:
         graph.add_node(node.id, color=node.color, label=node.val)
         if node.left:
             graph.add_edge(node.id, node.left.id)
-            l = x - 1 / 2 ** layer
+            l = x - 1 / 2**layer
             pos[node.left.id] = (l, y - 1)
             l = add_edges(graph, node.left, pos, x=l, y=y - 1, layer=layer + 1)
         if node.right:
             graph.add_edge(node.id, node.right.id)
-            r = x + 1 / 2 ** layer
+            r = x + 1 / 2**layer
             pos[node.right.id] = (r, y - 1)
             r = add_edges(graph, node.right, pos, x=r, y=y - 1, layer=layer + 1)
     return graph
+
 
 def draw_tree(tree_root):
     tree = nx.DiGraph()
     pos = {tree_root.id: (0, 0)}
     tree = add_edges(tree, tree_root, pos)
 
-    colors = [node[1]['color'] for node in tree.nodes(data=True)]
-    labels = {node[0]: node[1]['label'] for node in tree.nodes(data=True)}
+    colors = [node[1]["color"] for node in tree.nodes(data=True)]
+    labels = {node[0]: node[1]["label"] for node in tree.nodes(data=True)}
 
     plt.figure(figsize=(8, 5))
-    nx.draw(tree, pos=pos, labels=labels, arrows=False, node_size=2500, node_color=colors, font_color='white')
+    nx.draw(
+        tree,
+        pos=pos,
+        labels=labels,
+        arrows=False,
+        node_size=2500,
+        node_color=colors,
+        font_color="white",
+    )
     plt.show()
+
 
 def dfs_with_color_change(node, depth=0, visited=None):
     if visited is None:
@@ -47,14 +59,17 @@ def dfs_with_color_change(node, depth=0, visited=None):
         print(f"Visiting Node {node.val}")
         hue = 200.0  # Yellow hue
         saturation = 1.0  # Full saturation
-        value = 1.0 - (len(visited) * 0.19)  # Gradually decrease brightness based on traversal order
+        value = 1.0 - (
+            len(visited) * 0.19
+        )  # Gradually decrease brightness based on traversal order
         alpha = 0.99
         r, g, b = hsv_to_rgb(hue / 360, saturation, value)
         node.color = (r, g, b, alpha)
         visited.add(node)
         dfs_with_color_change(node.left, depth + 1, visited)
         dfs_with_color_change(node.right, depth + 1, visited)
-        
+
+
 def bfs_with_color_change(node):
     if node is None:
         return
@@ -66,23 +81,26 @@ def bfs_with_color_change(node):
     while queue:
         # Increment step counter for each BFS step
         step += 1
-        
+
         current, depth = queue.popleft()  # Dequeue a node and its depth
         if current not in visited:
             print(f"Visiting Node {current.val}")
             hue = 200.0  # Yellow hue
             saturation = 1.0  # Full saturation
-            value = 1.0 - (step * 0.16)  # Gradually decrease brightness based on traversal step
+            value = 1.0 - (
+                step * 0.16
+            )  # Gradually decrease brightness based on traversal step
             alpha = 1.0
             r, g, b = hsv_to_rgb(hue / 360, saturation, value)
             current.color = (r, g, b, alpha)
             visited.add(current)  # Mark the node as visited
-            
+
             # Enqueue child nodes with increased depth
             if current.left:
                 queue.append((current.left, depth + 1))
             if current.right:
                 queue.append((current.right, depth + 1))
+
 
 # Create the tree
 root = Node(0)
